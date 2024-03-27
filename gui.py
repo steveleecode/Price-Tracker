78ab067fd8251532303db713c974e61ddbb9c247
LEE <s-stlee@lwsd.org>
2024-03-27 11:50:25 -0700

    Worked on Gui

--- gui.py
+++ gui.py
@@ -1,25 +1,34 @@
 import tkinter as tk
 import tkinter.font as font
+from tkinter import *
 
 # Create the main application window
 root = tk.Tk()
 root.title("Amazon Price Tracker")  # Set the title of the window
 root.size()
 root.geometry("600x400")
+root.resizable(False, False)
 
-f = font.Font(size=25)
+title = font.Font(size=25)
+body = font.Font(size=12)
 
 # Create a label widget
-label = tk.Label(root, text="Amazon Price Tracker", font=f)
-label.pack()  # Use pack() method to add the label to the window
+label = tk.Label(root, text="Amazon Price Tracker", font=title)
+label.pack(anchor='center')  # Use pack() method to add the label to the window
+
+label = tk.Label(root, text = "Input URL to an Amazon Product", font=body)
+label.place(x=183, y=70)
+
+url = Entry(root, width = 90)
+url.place(x=27, y=100)
 
 # Create a function to handle button click event
 def button_click():
-    label.config(text="Button clicked!")
+    priceTrack()
 
 # Create a button widget
 button = tk.Button(root, text="Click Me", command=button_click)
-button.pack()  # Add the button to the window
+button.place(x=100, y=300)  # Add the button to the window
 
 # Run the Tkinter event loop
 root.mainloop()
